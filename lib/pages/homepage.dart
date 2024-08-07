5f95bf153cba61523791f513096d11363f06083d
mayamorena <maya.grau@myhsba.de>
2024-08-07 17:13:58 +0200

    version 1.0 of App Bar - header (frontend)

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,29 +6,61 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      // Start app bar
+      appBar: AppBar(
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.start, // Align children at the start of the cross axis (left)
           children: [
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "PosiTeams",
-                  style: TextStyle(
-                    fontFamily: 'futura Condensed',
-                    fontSize: 32,
-                    fontWeight: FontWeight.w500,
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Posi',
+                    style: TextStyle(
+                      fontFamily: 'futura Condensed',
+                      fontSize: 32,
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 7, 110, 23), // Green color for "Posi"
+                    ),
                   ),
-                ),
-              ],
+                  TextSpan(
+                    text: 'Teams',
+                    style: TextStyle(
+                      fontFamily: 'futura Condensed',
+                      fontSize: 32,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black, // Black color for "Teams"
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.swap_vert, size: 35), // Sort icon
+            onPressed: () {
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 12.0),
+            child: IconButton(
+              icon: Icon(Icons.filter_list, size: 35), // Filter icon
+              onPressed: () {
+              },
+            ),
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0), // Height of the bottom border
+          child: Container(
+            color: Color.fromARGB(255, 229, 229, 229), // Color of the bottom border
+            height: 1.0, // Thickness of the bottom border
+          ),
         ),
       ),
     );
   }
 }
+
