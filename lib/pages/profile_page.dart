963aaa4df17a6fc7df02517b58958921aece36f1
kvdthuesen <kirian.vonderthuesen@myhsba.de>
2024-09-10 15:23:52 +0200

    changing from stateless to statefull widgets, editing comments

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:positeams_programmierung2/pages/home_page.dart';
 import 'package:positeams_programmierung2/components/appbar.dart';
 import 'package:positeams_programmierung2/components/post.dart';
 import 'package:positeams_programmierung2/components/navigationbar.dart';
 
-class MyProfile extends StatelessWidget {
+class MyProfile extends StatefulWidget {
   const MyProfile({super.key});
+
+  @override
+  _MyProfileState createState() => _MyProfileState();
+}
+
+class _MyProfileState extends State<MyProfile> {
+  // Track which button is selected: 'Beiträge' or 'Reaktionen'
+  String _selectedButton = 'Beiträge'; // Default selected button
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white, // entire background white !!!
       appBar: MyAppBar(
         title: 'PosiTeams',
-        titleAlign: TextAlign.center,
-        leading: IconButton(
-          icon: const Icon(Icons.close, size: 30),
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => const Homepage()),
-            );
-          },
-        ),
+        titleAlign: TextAlign.start,
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 16.0),
@@ -34,106 +33,123 @@ class MyProfile extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          // Non-scrollable part
-          Stack(
-            clipBehavior: Clip.none, // allows the avatar to overflow the stack
-            children: [
-              // Header image
-              Container(
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5.0),
-                  image: const DecorationImage(
-                    image: AssetImage('lib/images/hermes.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              // Profile picture (floating above the header image)
-              Positioned(
-                left: 24, // Positioning avatar
-                bottom: -40, // Pulling avatar up
-                child: const CircleAvatar(
-                  radius: 45,
-                  backgroundColor: Colors.white,
-                  child: CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage('lib/images/avatar.jpg'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 40), // Space to accommodate the floating avatar
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: CustomScrollView(
+        slivers: [
+          // Sliver for profile header, image, and avatar
+          SliverToBoxAdapter(
+            child: Stack(
+              clipBehavior: Clip.none, // allows the avatar to overflow the stack - bc white background
               children: [
-                // Name and description
+                // Header image
                 Container(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        'Maya -',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: 'Futura',
-                        ),
-                      ),
-                      Text(
-                        'Team Data Science (Business Intelligence)',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                          fontFamily: 'Futura',
-                        ),
-                      ),
-                    ],
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0),
+                    image: const DecorationImage(
+                      image: AssetImage('lib/images/hermes.jpg'),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 10), // space between the text and divider
-                const Divider( // Divider between the text and buttons
-                  color: Colors.grey,
-                  thickness: 0.5,
-                  indent: 3, // Left margin
-                  endIndent: 3, // Right margin
-                ),
-                const SizedBox(height: 5), // space between divider and buttons
-                // Buttons for "Beiträge" and "Reaktionen"
-                Row(
-                  children: [
-                    _buildProfileButton(
-                      text: 'Beiträge',
-                      onPressed: () {},
+                // Profile picture (floating above the header image)
+                Positioned(
+                  left: 24, // Positioning avatar
+                  bottom: -40, // Pulling avatar up
+                  child: const CircleAvatar(
+                    radius: 45,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage('lib/images/avatar.jpg'),
                     ),
-                    const SizedBox(width: 10), // Small gap between the buttons
-                    _buildProfileButton(
-                      text: 'Reaktionen',
-                      onPressed: () {},
-                    ),
-                  ],
+                  ),
                 ),
               ],
             ),
           ),
-          const SizedBox(height: 20), // Add space between the buttons and class "post"
-          // Scrollable part starting with class "post"
-          Expanded(
-            child: SingleChildScrollView(
-              padding: EdgeInsets.zero, // Remove any default padding
+          SliverToBoxAdapter(
+            child: const SizedBox(height: 40),
+          ),
+          // Sliver for the name and description
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: const [
-                  Post(),
-                  Post(),
-                  Post(),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Maya -',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: 'Futura',
+                          ),
+                        ),
+                        Text(
+                          'Team Data Science (Business Intelligence)',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                            fontFamily: 'Futura',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10), // space between the text and divider
+                  const Divider( // Divider between the text and buttons
+                    color: Colors.grey,
+                    thickness: 0.5,
+                    height: 1,
+                    indent: 3, // Left margin
+                    endIndent: 3, // Right margin
+                  ),
                 ],
               ),
+            ),
+          ),
+          // Regular SliverToBoxAdapter for buttons (not pinned)
+          SliverToBoxAdapter(
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  _buildProfileButton(
+                    text: 'Beiträge',
+                    isSelected: _selectedButton == 'Beiträge',
+                    onPressed: () {
+                      setState(() {
+                        _selectedButton = 'Beiträge';
+                      });
+                    },
+                  ),
+                  const SizedBox(width: 10), // gap between the buttons
+                  _buildProfileButton(
+                    text: 'Reaktionen',
+                    isSelected: _selectedButton == 'Reaktionen',
+                    onPressed: () {
+                      setState(() {
+                        _selectedButton = 'Reaktionen';
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Sliver for posts content
+          SliverList(
+            delegate: SliverChildListDelegate(
+              const [
+                Post(),
+                Post(),
+                Post(),
+              ],
             ),
           ),
         ],
@@ -142,15 +158,19 @@ class MyProfile extends StatelessWidget {
     );
   }
 
-  // Method to build profile buttons to avoid repetition !!!
-  Widget _buildProfileButton({required String text, required VoidCallback onPressed}) {
+  // Method to build profile buttons with color change based on selection
+  Widget _buildProfileButton({
+    required String text,
+    required bool isSelected,
+    required VoidCallback onPressed,
+  }) {
     return Expanded(
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-          backgroundColor: const Color.fromARGB(100, 220, 220, 220),
-          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8), // Reduce vertical padding for a shorter button
-          minimumSize: const Size(0, 36), // Set a minimum height
+          backgroundColor: const Color.fromARGB(255, 220, 220, 220),
+          padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+          minimumSize: const Size(0, 30),
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(5.0),
@@ -159,9 +179,9 @@ class MyProfile extends StatelessWidget {
         ),
         child: Text(
           text,
-          style: const TextStyle(
+          style: TextStyle(
             fontSize: 16,
-            color: Colors.black,
+            color: isSelected ? Colors.black : Colors.grey, // Change color based on selection
             fontFamily: 'Futura',
           ),
         ),
@@ -169,3 +189,4 @@ class MyProfile extends StatelessWidget {
     );
   }
 }
+
