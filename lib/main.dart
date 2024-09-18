58f2c8c9ded91bccc37763fcdaf42f01d9180cf8
kvdthuesen <kirian.vonderthuesen@myhsba.de>
2024-09-18 15:10:14 +0200

    firebase hotfix

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 
-void main() async{
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
